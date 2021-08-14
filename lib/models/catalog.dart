b8c2df5b70176c8faf27d5f8433b007cef8c6d1b
Dilip <dkp67853@gmail.com>
2021-08-14 14:24:13 +0530

    day10 complete

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,18 @@
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "iPhone 12 Pro",
+        desc: "abc",
+        price: 222222,
+        color: "#33505a",
+        image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx9gmr41b9LAAZuIRRmoObj6M_kAhfH4DwIm9ik_FK6FLmERuTOpDYa5bg3E9JRstHXLw3xrL7&usqp=CAc")
+  ];
+}
+
 class Item {
-  final String id;
+  final num id;
   final String name;
   final String desc;
   final num price;
@@ -14,13 +27,3 @@ class Item {
       required this.color,
       required this.image});
 }
-
-final products = [
-  Item(
-      id: "dilip001",
-      name: "iPhone 12 Pro",
-      desc: "abc",
-      price: 222222,
-      color: "#33505a",
-      image: "")
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project1/models/catalog.dart';
 import 'package:flutter_project1/widgets/drawer.dart';
+import 'package:flutter_project1/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    //dummylist is created to make a list of an item if you dont have many
+    final dummyList = List.generate(15, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome"),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+          itemCount: dummyList.length,
         ),
       ),
       drawer: MyDrawer(),
